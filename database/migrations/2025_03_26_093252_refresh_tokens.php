1075b1b0f574c5aeff3f60983e017dfa3662d064
Thu Zin Htet <thuzinhtet.dev@gmail.com>
2025-04-02 18:27:55 +0630

    fix access token expired time to one minute

--- database/migrations/2025_03_26_093252_refresh_tokens.php
+++ database/migrations/2025_03_26_093252_refresh_tokens.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->string('refresh_token')->unique();
             $table->timestamp('expired_at')->default(now()->addMinutes(1));
-            $table->timestamps();
-        });
+            $table->timestamps();   
+        });     
     }
 
     /**
